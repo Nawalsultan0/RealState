90c737698b08c4e72cb3f20cbc844a7989a69241
Nawal <maham22.sultan2002@gmail.com>
2026-06-13 22:05:13 +0500

    Backend

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
            <Link to='/about'>
           <li className='  hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-          <Link to='Sign-in' >
+          <Link to='Sign-in' >  
           <li className='  text-slate-700 hover:underline'>Sign in</li>
           </Link>
          </ul>
